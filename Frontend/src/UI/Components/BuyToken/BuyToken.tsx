dc379719026576eaf92ef74aa49fbd32b739da99
Matvey_Makarski <matveymakarski@gmail.com>
2024-06-11 02:11:35 +0300

    create ethereum price

--- Frontend/src/UI/Components/BuyToken/BuyToken.tsx
+++ Frontend/src/UI/Components/BuyToken/BuyToken.tsx
@@ -12,6 +12,44 @@ export const BuyToken = () => {
 
     const {user, transact, getTransact, balance} = useContext(Context);
 
+    const [ethPrice, setEthPrice] = useState<number>(0);
+
+    const [tokenUsdPrice, setTokenUsdPrice] = useState<number>(0)
+ 
+    useEffect(() => {
+      (async () => {
+        fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+        .then(response => response.json())
+        .then(data => {
+            const ethPriceInUsdt = data.ethereum.usd;
+            console.log(ethPriceInUsdt); // Вывод цены Ethereum в USDT
+            setEthPrice(ethPriceInUsdt)
+            console.log(ethPrice)
+        })
+        .catch(error => {
+            console.error('Ошибка при получении цены Ethereum в USDT:', error);
+        });
+        const priceData = await Service.viewTokenPrice()
+        const tokenPriceInEth = priceData / 10**18
+
+        const tokenPriceInUsdt = tokenPriceInEth * ethPrice;
+
+        setTokenUsdPrice(tokenPriceInUsdt);
+        console.log(tokenUsdPrice);
+      })();
+    }, []);
+
+    useEffect(() => {
+      (async () => {
+        const priceData = await Service.viewTokenPrice()
+        const tokenPriceInEth = priceData / 10**18
+
+        const tokenPriceInUsdt = tokenPriceInEth * ethPrice;
+
+        setTokenUsdPrice(tokenPriceInUsdt);
+        console.log(tokenUsdPrice);
+      })();
+    }, [ethPrice]);
 
     const buyTokenHandler = async(e: any) =>{
         e.preventDefault();
@@ -36,6 +74,8 @@ export const BuyToken = () => {
         user != '' ?
         <form className="mt-5 mb-5 flex flex-col items-center" onSubmit={buyTokenHandler}>
         <h1 className='text-3xl font-semibold text-gray-900 dark:text-white flex flex-col items-center'>Ваш баланс токенов: {balance / 10000} LED</h1>
+        <h1 className='text-3xl font-semibold text-gray-900 dark:text-white mt-5 mb-5 flex flex-col items-center'>Цена Ethereum: {ethPrice} USDT</h1>
+        <h1 className='text-3xl font-semibold text-gray-900 dark:text-white mb-5 flex flex-col items-center'>Цена LED: {tokenUsdPrice} USDT</h1>
           <div className="mt-5 mb-5">
             <h1 className='text-3xl font-semibold text-gray-900 dark:text-white mt-5 mb-5 flex flex-col items-center'>Купить токены</h1>
             <input type="number" id="description" className="mt-7 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Количество токенов" required />
